9439b8dc9115ff46a8e52618eb4dfc644f7b4b85
antoine <antoine.chebel@gmail.com>
2021-12-03 15:47:46 +0100

    création d'un nouveau projet
    ajout d'une page d'acceuil
    ajout d'un hello word en php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'acceuil</title>
+</head>
+<body>
+<h1>Page d'acceuil</h1> 
+<p>
+<?php
+echo 'du PHP dans du HTML <br>';
+echo '(en réalité il ne faut surtout pas le faire.) <br>';
+?>   
+</p>
+</body>
+</html>
