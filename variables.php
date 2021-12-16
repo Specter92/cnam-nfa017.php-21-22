45da7606299b30c70d2be19c7e422eb9b7ceb385
antoine <antoine.chebel@gmail.com>
2021-12-16 11:44:26 +0100

    Amélioration du code
    - ajout de code sur les variables
    - ajout de code dans le hello world

---
+++ variables.php
@@ -0,0 +1,38 @@
+<?php
+
+//affectation d'une chaine a une variable
+$myVar1 = 'je suis une chaine de charactères<br>';
+
+echo $myVar1;
+//réaffectation d'un nombre entier à une variable
+$myVar1 = 123;
+
+echo $myVar1;
+echo '<br>';
+
+//affectatation de valeur booléénnes à des variables
+$isMorning = true;
+$isSunny = false;
+
+
+echo $isMorning;
+echo '<br>';
+echo $isSunny;
+echo '<br>';
+
+//affectation de nombre à virgules flottante à des variables
+$pi = 3.14;
+$e = 2.71;
+
+echo $e;
+echo '<br>';
+echo $pi;
+echo '<br>';
+
+
+// affectation d'une valeur nulle
+$referundum = null;
+
+echo $referundum;
+echo '<br>';
+
