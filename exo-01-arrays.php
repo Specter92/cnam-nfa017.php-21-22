284906350c79f160c9da7c9ba9e13266ab6b91f6
antoine <antoine.chebel@gmail.com>
2022-01-07 10:36:49 +0100

    Ajout du code
    - exercice 1 fait avec dump

---
+++ exo-01-arrays.php
@@ -0,0 +1,22 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+dump(__DIR__);
+
+// créer un tableau contenant 5 cartes d'un jeu de 32 cartes à jouer.
+//Exemple de nom de cartes : 'As de pique', '3carreaux', 'valet de treffle', etc..
+$cartes = ["AS", "carreau", "valet", "pique", "treffle"];
+dump($cartes);
+
+// Tirez un nombre "n" au hasard, compris entre 1 et 5 inclus.
+$tirerCarte = array_splice($cartes,0, 5);
+dump($tirerCarte);
+
+// A l'aide de ce nombre, retirer la n-ieme carte de la main.
+$eniemeCarte = array_pop($tirerCarte);
+
+// Afficher le nom de la carte qui a été retirée.
+echo "la carte {$eniemeCarte} a été retiré <br>\n";
+
+
